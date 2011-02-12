463083ac4f7f65e78c8f32a2eed4288ab73decb4
Phil Rigovanov <phil@rigovanov.ru>
2011-02-12 16:51:59 +0300

    ready

--- locales_merge.rb
+++ locales_merge.rb
@@ -1,21 +1,22 @@
 # encoding: UTF-8
 class Hash
-  # Returns a new hash with +self+ and +other_hash+ merged recursively.
   def deep_merge(other_hash)
     dup.deep_merge!(other_hash)
   end
 
-  # Returns a new hash with +self+ and +other_hash+ merged recursively.
-  # Modifies the receiver in place.
   def deep_merge!(other_hash)
     other_hash.each_pair do |k,v|
       tv = self[k]
-      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.deep_merge(v) : v
+      if tv.is_a?(Hash) then
+        self[k] =  v.is_a?(Hash) ? tv.deep_merge(v) : tv
+      else
+        self[k] = v
+      end
     end
     self
   end
 end
 
-ruI18n = {:hey => "Привет", :bye => "Пока"}
-enI18n = {:hey => "Hey", :bye => "Bye", :cava => "Ca va"}
+ruI18n = {:hey => "Привет", :bye => "Пока", :oh =>"Эх"}
+enI18n = {:hey => {:morning => "Good morning", :day => "Good day", :evening => {:early => "Good evening", :late => "Good night"}}, :bye => "Bye", :cava => "Ca va"}
 enI18n.deep_merge(ruI18n).each { |k,v| puts "#{k} => #{v}"}
